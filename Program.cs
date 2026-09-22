167424271e68bfa7e098e2d24e9802e02d33ef21
AdrianKebab <adrian.naslund05@gmail.com>
2026-09-22 13:21:35 +0200

    add code we probablt need

--- Program.cs
+++ Program.cs
@@ -1,6 +1,38 @@
+using OpenAI;
+using OpenAI.Responses;
+using System.ClientModel;
+using DotNetEnv;
+
+#pragma warning disable OPENAI001
+
+Env.Load();
+
+const string deploymentName = "gpt-5.4-mini";
+var endpoint = Environment.GetEnvironmentVariable("ENDPOINT") ?? throw new Exception("fel endpoint");
+var apiKey = Environment.GetEnvironmentVariable("KEY") ?? throw new Exception("fel key");
+
+var client = new ResponsesClient(
+    new ApiKeyCredential(apiKey),
+    new ResponsesClientOptions
+    {
+        Endpoint = new Uri(endpoint)
+    });
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+ResponseResult response = client.CreateResponse(
+    deploymentName,
+    $"""
+    Du ska skriva om alla ord från inmatningen till "Kebab"
+    input pls
+    """
+);
+
+//Console.WriteLine(response.GetOutputText());
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.Run();
